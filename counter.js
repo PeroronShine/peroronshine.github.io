d8c575d9432e2000325252288ba7eeabaa70003f
PeroronShine <148922733+PeroronShine@users.noreply.github.com>
2024-06-30 19:05:01 +1000

    Update and rename counter.js to Rickroll

--- counter.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let cnt = localStorage.getItem('visitCount');
-    if (cnt === null) { cnt = 0; };
-    cnt++;
-    localStorage.setItem('visitCount', cnt);
-    document.getElementById('counter').innerText = cnt;
-});
