b05e837c009891466e44e0b18d43b0e1c3074ba2
PeroronShine <148922733+PeroronShine@users.noreply.github.com>
2024-06-30 18:48:33 +1000

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -328,15 +328,6 @@ const MoyyuevaUMinit = (function() {
     return {
         AppInit: AppInit
     };
-
-    document.addEventListener('DOMContentLoaded', function () {
-        let cnt = localStorage.getItem('visitCount');
-        if (cnt === null) { cnt = 0; };
-        cnt++;
-        localStorage.setItem('visitCount', cnt);
-        document.getElementById('counter').innerText = cnt;
-    });
-    
 }());
 
 MottuevaUMinit.AppInit();
